e4f9d008b3711b7ba99f5e707173bc7de16f5261
Sune Kirkeby <sune.kirkeby@vivino.com>
2021-11-24 10:29:10 +0100

    One.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kirkeby/flargh
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0 // indirect
---
+++ one/one_test.go
@@ -0,0 +1,11 @@
+package one
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFest(t *testing.T) {
+	assert.Equal(t, 42, theAnswer)
+}
